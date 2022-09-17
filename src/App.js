ae3740adcca088cd681e1085828557b7cb25a307
mugeshsamraj <mugeshsamraj1996@gmail.com>
2022-09-17 07:59:04 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Sections/Home/Home';  
+import About from './Sections/About/About';
+import Header from './Sections/Header/Header';
+import Contact from './Sections/Contact/Contact';
+import Project from './Sections/Project/Project';
+import Footer from './Sections/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <Header/>
+      <Home/>
+      <About/>
+      <Project/>
+      <Contact/>
+      <Footer/>
+    </>
   );
 }
 
